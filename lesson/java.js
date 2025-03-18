f919e32f1d95fde14ca9d932302b2b63546aa118
lateef123442 <umuyetuahmed@gmail.com>
2025-03-18 13:05:03 +0100

    Add files via upload

---
+++ lesson/java.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function(){
+    //heroslider//
+    $("#hero-slider").owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        items: 1,
+        dots:false,
+        smartSpeed:1000,
+        navText:['PREV' , 'NEXT'],
+        responsive:{
+            0:{
+             
+            },
+            600:{
+             
+            },
+            1000:{
+               
+            }
+        }
+    })
+
+});
+
