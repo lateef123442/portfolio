268e1ab3e25b7e70e79c1cb5325af25938a648ea
lateef123442 <umuyetuahmed@gmail.com>
2025-03-18 15:57:11 +0100

    Add files via upload

---
+++ reciept/java.js
@@ -0,0 +1,116 @@
+
+function total()
+{
+  
+  var A=document.getElementById("qty1").value;
+  var B=document.getElementById("rat1").value;
+  var C=A*B
+  document.getElementById("amt1").value=C;
+
+
+  var D=document.getElementById("qty2").value;
+  var E=document.getElementById("rat2").value;
+  var F=D*E
+  document.getElementById("amt2").value=F;
+
+
+  var G=document.getElementById("qty3").value;
+  var H=document.getElementById("rat3").value;
+  var I=G*H
+  document.getElementById("amt3").value=I;
+
+  var J=document.getElementById("qty4").value;
+  var K=document.getElementById("rat4").value;
+  var L=J*K
+  document.getElementById("amt4").value=L;
+
+  var M=document.getElementById("qty5").value;
+  var N=document.getElementById("rat5").value;
+  var O=M*N
+  document.getElementById("amt5").value=O;
+
+  var P=document.getElementById("qty6").value;
+  var Q=document.getElementById("rat6").value;
+  var R=P*Q
+document.getElementById("amt6").value=R;
+  
+
+
+var S=document.getElementById("qty7").value;
+  var T=document.getElementById("rat7").value;
+  var U=S*T
+document.getElementById("amt7").value=U;
+  
+
+
+var V=document.getElementById("qty8").value;
+  var W=document.getElementById("rat8").value;
+  var X=V*W
+document.getElementById("amt8").value=X;
+
+
+var Y=document.getElementById("qty9").value;
+  var Z=document.getElementById("rat9").value;
+  var AA=Y*Z
+document.getElementById("amt9").value=AA;
+
+  var AB=C+F+I+L+O+R+U+X+AA;
+  document.getElementById("total").value=AB;
+}
+function update_lt1()
+
+  {
+    var A=document.getElementById("qty1").value;
+    var B=document.getElementById("rat1").value;
+    var C=A*B
+    document.getElementById("amt1").value=C;
+  
+
+ 
+    var D=document.getElementById("qty2").value;
+  var E=document.getElementById("rat2").value;
+  var F=D*E
+  document.getElementById("amt2").value=F;
+
+
+  
+  var G=document.getElementById("qty3").value;
+  var H=document.getElementById("rat3").value;
+  var I=G*H
+  document.getElementById("amt3").value=I;
+
+  var J=document.getElementById("qty4").value;
+  var K=document.getElementById("rat4").value;
+  var L=J*K
+  document.getElementById("amt4").value=L;
+
+  var M=document.getElementById("qty5").value;
+  var N=document.getElementById("rat5").value;
+  var O=M*N
+  document.getElementById("amt5").value=O;
+
+  var P=document.getElementById("qty6").value;
+  var Q=document.getElementById("rat6").value;
+  var R=P*Q
+document.getElementById("amt6").value=R;
+  
+
+
+var S=document.getElementById("qty7").value;
+  var T=document.getElementById("rat7").value;
+  var U=S*T
+document.getElementById("amt7").value=U;
+  
+
+
+var V=document.getElementById("qty8").value;
+  var W=document.getElementById("rat8").value;
+  var X=V*W
+document.getElementById("amt8").value=X;
+
+
+var Y=document.getElementById("qty9").value;
+  var Z=document.getElementById("rat9").value;
+  var AA=Y*Z
+document.getElementById("amt9").value=AA;
+    }
