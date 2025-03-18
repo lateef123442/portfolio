067f62430685304f18ee6bdb8c56df5a9d741eb3
lateef123442 <umuyetuahmed@gmail.com>
2025-03-18 12:28:12 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,12 @@
+var tablinks = document.getElementsByClassName("tab-link");
+var tabcontents = document.getElementsByClassName("tab-content");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove(active-link)
+    }
+    for(tabcontent of contents){
+        tablink.classlist.remove(active-tab)
+    }
+
+}
